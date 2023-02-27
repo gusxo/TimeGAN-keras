fac01ffa054f9b36a9b1d58235d86440223efaed
gusxo <chlgus1233@gmail.com>
2023-02-27 02:21:37 +0000

    update timegan.py : add tf.function decorator, input is convert to tensor before using

--- timegan.py
+++ timegan.py
@@ -3,15 +3,15 @@ import tensorflow.keras as keras
 import numpy as np
 from tqdm import trange
 
-def get_model(input_shape, output_units, rnn_units, layer_cnt):
-    inputs = keras.layers.Input(input_shape)
-    x = inputs
-    for i in range(layer_cnt):
-        x = keras.layers.GRU(rnn_units, return_sequences=True)(x)
-    outputs = keras.layers.Dense(output_units, activation="sigmoid")(x)
-    return keras.Model(inputs, outputs)
-
 def timegan_init(time_series_len, features, rnn_units, rnn_layers):
+    def get_model(input_shape, output_units, rnn_units, layer_cnt):
+        inputs = keras.layers.Input(input_shape)
+        x = inputs
+        for _ in range(layer_cnt):
+            x = keras.layers.GRU(rnn_units, return_sequences=True)(x)
+        outputs = keras.layers.Dense(output_units, activation="sigmoid")(x)
+        return keras.Model(inputs, outputs)
+
     input_shape = (time_series_len, features)
     latent_code_shape = (time_series_len, rnn_units)
     embedder = get_model(input_shape, rnn_units, rnn_units, rnn_layers)
@@ -30,12 +30,10 @@ def timegan_export_generator(timegan_tuple):
     return keras.Model(syn_gen_input, syn_gen_output)
     
 def timegan_train(x, timegan_tuple, epochs, batch_size, learning_rate):
-    embedder, generator, supervisor, recovery, discriminator = timegan_tuple
-
     #convert to float32(because random_vector's type is float32, should be matched)
     x = x.astype(np.float32)
-    get_batch = lambda : x[np.random.permutation(x.shape[0])[:batch_size]]
-    get_random_vector = lambda : np.random.uniform(size=(batch_size, x.shape[1], x.shape[2]))
+    get_batch = lambda : tf.convert_to_tensor(x[np.random.permutation(x.shape[0])[:batch_size]])
+    get_random_vector = lambda : tf.convert_to_tensor(np.random.uniform(size=(batch_size, x.shape[1], x.shape[2])))
 
     #loss & optimizer
     mse = keras.losses.MeanSquaredError()
@@ -46,113 +44,114 @@ def timegan_train(x, timegan_tuple, epochs, batch_size, learning_rate):
     opt_embedder = keras.optimizers.Adam(learning_rate=learning_rate)
     opt_discriminator = keras.optimizers.Adam(learning_rate=learning_rate)
 
-    #train autoencoder
-    print(f"train autoencoder")
-    for _ in trange(epochs):
-        batch = get_batch()
+    #training functions
+    @tf.function
+    def train_autoencoder(x, timegan, mse, opt):
+        embedder, generator, supervisor, recovery, discriminator = timegan
         with tf.GradientTape() as tape:
-            y_true = embedder(batch)
+            y_true = embedder(x)
             y_true = recovery(y_true)
-            loss = 10 * tf.sqrt(mse(y_true, batch))
+            loss = 10 * tf.sqrt(mse(y_true, x))
         var_list = embedder.trainable_variables + recovery.trainable_variables
         gradients = tape.gradient(loss, var_list)
-        opt_autoencoder.apply_gradients(zip(gradients, var_list))
+        opt.apply_gradients(zip(gradients, var_list))
+        return loss
 
-    #train supervisor
-    print(f"train supervisor")
-    for _ in trange(epochs):
-        batch = get_batch()
+    @tf.function
+    def train_supervisor(x, timegan, mse, opt):
+        embedder, generator, supervisor, recovery, discriminator = timegan
         with tf.GradientTape() as tape:
-            y_true = embedder(batch)
+            y_true = embedder(x)
             y_pred = supervisor(y_true)
             loss = mse(y_true[:, 1:, :], y_pred[:, :-1, :])
         var_list = generator.trainable_variables + supervisor.trainable_variables
         gradients = tape.gradient(loss, var_list)
         apply_grads = [(grad, var) for (grad, var) in zip(gradients, var_list) if grad is not None]
-        opt_supervisor.apply_gradients(apply_grads)
+        opt.apply_gradients(apply_grads)
+        return loss
 
-    #joint train
-    print(f"joint train")
-    for _ in trange(epochs):
-        for __ in range(2):
-            batch = get_batch()
-            random_vector = get_random_vector()
-            #train generator
-            with tf.GradientTape() as tape:
-                #supervised loss
-                y_true = embedder(batch)
-                y_pred = supervisor(y_true)
-                supervised_loss = mse(y_true[:, 1:, :], y_pred[:, :-1, :])
+    @tf.function
+    def train_generator(x, z, timegan, mse, bce, opt):
+        embedder, generator, supervisor, recovery, discriminator = timegan
+        with tf.GradientTape() as tape:
+            #supervised loss
+            y_true = embedder(x)
+            y_pred = supervisor(y_true)
+            supervised_loss = mse(y_true[:, 1:, :], y_pred[:, :-1, :])
 
-                #unsupervised loss
-                y_true = tf.ones((batch_size, x.shape[1], 1))
-                y_pred = generator(random_vector)
-                y_pred = supervisor(y_pred)
-                y_pred = discriminator(y_pred)
-                unsupervised_loss = bce(y_true, y_pred)
+            #unsupervised loss
+            y_true = tf.ones((x.shape[0], x.shape[1], 1))
+            y_pred = generator(z)
+            y_pred = supervisor(y_pred)
+            y_pred = discriminator(y_pred)
+            unsupervised_loss = bce(y_true, y_pred)
 
-                #unsupervised loss - E
-                y_true = tf.ones((batch_size, x.shape[1], 1))
-                y_pred = generator(random_vector)
-                y_pred = discriminator(y_pred)
-                unsupervised_loss_e = bce(y_true, y_pred)
+            #unsupervised loss - E
+            y_true = tf.ones((x.shape[0], x.shape[1], 1))
+            y_pred = generator(z)
+            y_pred = discriminator(y_pred)
+            unsupervised_loss_e = bce(y_true, y_pred)
 
-                #moment loss
-                y_true = batch
-                y_pred = generator(random_vector)
-                y_pred = supervisor(y_pred)
-                y_pred = recovery(y_pred)
-                y_true_mean, y_true_var = tf.nn.moments(y_true, axes=[0])
-                y_pred_mean, y_pred_var = tf.nn.moments(y_pred, axes=[0])
-                v1 = tf.reduce_mean(tf.abs(y_true_mean - y_pred_mean))
-                v2 = tf.reduce_mean(tf.abs(tf.sqrt(y_true_var + 1e-6) - tf.sqrt(y_pred_var + 1e-6)))
-                moment_loss = v1 + v2
+            #moment loss
+            y_true = x
+            y_pred = generator(z)
+            y_pred = supervisor(y_pred)
+            y_pred = recovery(y_pred)
+            y_true_mean, y_true_var = tf.nn.moments(y_true, axes=[0])
+            y_pred_mean, y_pred_var = tf.nn.moments(y_pred, axes=[0])
+            v1 = tf.reduce_mean(tf.abs(y_true_mean - y_pred_mean))
+            v2 = tf.reduce_mean(tf.abs(tf.sqrt(y_true_var + 1e-6) - tf.sqrt(y_pred_var + 1e-6)))
+            moment_loss = v1 + v2
 
-                loss = supervised_loss + 100 * tf.sqrt(unsupervised_loss) + unsupervised_loss_e + 100 * moment_loss
+            loss = supervised_loss + 100 * tf.sqrt(unsupervised_loss) + unsupervised_loss_e + 100 * moment_loss
 
-            var_list = generator.trainable_variables + supervisor.trainable_variables
-            gradients = tape.gradient(loss, var_list)
-            opt_generator.apply_gradients(zip(gradients, var_list))
+        var_list = generator.trainable_variables + supervisor.trainable_variables
+        gradients = tape.gradient(loss, var_list)
+        opt.apply_gradients(zip(gradients, var_list))
+        return loss
 
-            #train embedder
-            with tf.GradientTape() as tape:
-                #supervised loss
-                y_true = embedder(batch)
-                y_pred = supervisor(y_true)
-                supervised_loss = mse(y_true[:, 1:, :], y_pred[:, :-1, :])
+    @tf.function
+    def train_embedder(x, timegan, mse, opt):
+        embedder, generator, supervisor, recovery, discriminator = timegan
+        with tf.GradientTape() as tape:
+            #supervised loss
+            y_true = embedder(x)
+            y_pred = supervisor(y_true)
+            supervised_loss = mse(y_true[:, 1:, :], y_pred[:, :-1, :])
 
-                #reconstruction loss
-                y_true = embedder(batch)
-                y_true = recovery(y_true)
-                y_pred = batch
-                reconstruction_loss = 10 * tf.sqrt(mse(y_true, y_pred))
+            #reconstruction loss
+            y_true = embedder(x)
+            y_true = recovery(y_true)
+            y_pred = x
+            reconstruction_loss = 10 * tf.sqrt(mse(y_true, y_pred))
 
-                loss = reconstruction_loss + 0.1 * supervised_loss
+            loss = reconstruction_loss + 0.1 * supervised_loss
 
-            var_list = embedder.trainable_variables + recovery.trainable_variables
-            gradients = tape.gradient(loss, var_list)
-            opt_embedder.apply_gradients(zip(gradients, var_list))
+        var_list = embedder.trainable_variables + recovery.trainable_variables
+        gradients = tape.gradient(loss, var_list)
+        opt.apply_gradients(zip(gradients, var_list))
+        return loss
 
-        batch = get_batch()
-        random_vector = get_random_vector()
-        #train discriminator
+    @tf.function
+    def train_discriminator(x, z, timegan, bce, opt):
+        embedder, generator, supervisor, recovery, discriminator = timegan
         with tf.GradientTape() as tape:
             #loss on FN
-            y_true = tf.ones((batch_size, x.shape[1], 1))
-            y_pred = embedder(batch)
+            y_true = tf.ones((x.shape[0], x.shape[1], 1))
+            y_pred = embedder(x)
             y_pred = discriminator(y_pred)
             loss_on_FN = bce(y_true, y_pred)
 
             #loss on FP
-            y_true = tf.zeros((batch_size, x.shape[1], 1))
-            y_pred = generator(random_vector)
+            y_true = tf.zeros((x.shape[0], x.shape[1], 1))
+            y_pred = generator(z)
             y_pred = supervisor(y_pred)
             y_pred = discriminator(y_pred)
             loss_on_FP = bce(y_true, y_pred)
 
             #loss on FP - E
-            y_true = tf.zeros((batch_size, x.shape[1], 1))
-            y_pred = generator(random_vector)
+            y_true = tf.zeros((x.shape[0], x.shape[1], 1))
+            y_pred = generator(z)
             y_pred = discriminator(y_pred)
             loss_on_FP_E = bce(y_true, y_pred)           
 
@@ -160,7 +159,32 @@ def timegan_train(x, timegan_tuple, epochs, batch_size, learning_rate):
 
         var_list = discriminator.trainable_variables
         gradients = tape.gradient(loss, var_list)
-        opt_discriminator.apply_gradients(zip(gradients, var_list))
+        opt.apply_gradients(zip(gradients, var_list))
+        return loss
+
+    
+    #conduct train
+    print(f"train autoencoder")
+    for _ in trange(epochs):
+        batch = get_batch()
+        train_autoencoder(batch, timegan_tuple, mse, opt_autoencoder)
+
+    print(f"train supervisor")
+    for _ in trange(epochs):
+        batch = get_batch()
+        train_supervisor(batch, timegan_tuple, mse, opt_supervisor)
+
+    print(f"joint train")
+    for _ in trange(epochs):
+        for __ in range(2):
+            batch = get_batch()
+            random_vector = get_random_vector()
+            train_generator(batch, random_vector, timegan_tuple, mse, bce, opt_generator)
+            train_embedder(batch, timegan_tuple, mse, opt_embedder)
+
+        batch = get_batch()
+        random_vector = get_random_vector()
+        train_discriminator(batch, random_vector, timegan_tuple, bce, opt_discriminator)
 
     return timegan_tuple
 
